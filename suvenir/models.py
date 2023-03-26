b22c75a71d542c556ad4b33e197f1214a60f2434
chel <123@gmail.com>
2023-03-26 10:43:14 +0500

    v 1.3

--- suvenir/models.py
+++ suvenir/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+    price = models.FloatField()
+    img = models.ImageField(upload_to='images')
+    date = models.DateTimeField()
